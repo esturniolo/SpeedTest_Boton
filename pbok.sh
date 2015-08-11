1c78de8fbd73d7f46ed14dc61e05f6077b50d544
esturniolo <esturniolo@gmail.com>
2015-08-11 17:16:41 -0300

    All to English

--- pbok.sh
+++ pbok.sh
@@ -3,6 +3,6 @@
 #Use it as you want, be happy. Just remember me in every run :D
 
 API="Your_Pushbullet_API_here"
-MSG="La conexion de internet esta perfectas condiciones"
+MSG="Your internet connection it's ok. Download: $SPEEDDOWN Mbit/s / Upload: $SPEEDUP Mbit/s. Hooray!"
 
 curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="ATENCION - ISP" -d body="$MSG" > /dev/null
