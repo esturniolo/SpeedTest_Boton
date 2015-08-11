721dbcdf5256c39ab328979215bc2d220305ca74
esturniolo <esturniolo@gmail.com>
2015-08-11 17:05:41 -0300

    Vars and texts to English. More: See description
    Added download and upload speed to Pushbullet message.

--- pbbad.sh
+++ pbbad.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-API="Your_Pushbullet_API_here"
-MSG="Problemas de velocidad de bajada con mi ISP"
+DIRDEST=/your/dest/dir/watched/by/IFTTT
+DIRTEMP=/go/to/temp/dir #Same as speedtest_boton.sh
+FILETEMP=test_temporal.tmp
+SPEEDDOWN=`awk '$0=$2' FS='Download: ' RS='Mbit/s' $DIRTEMP/$FILETEMP`
+SPEEDDUP=`awk '$0=$2' FS='Upload: ' RS='Mbit/s' $DIRTEMP/$FILETEMP`
 
-curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="ALERTA - ISP" -d body="$MSG"
+API="Your_Pushbullet_API_here"
+MSG="My ISP speed sucks now! Download: $SPEEDDOWN Mbit/s / Upload: $SPEEDUP Mbit/s"
+
+curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="ALERT - ISP" -d body="$MSG"
+
