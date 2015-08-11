d674a0992ee623f6ee94e6a2c8b1445285412e75
esturniolo <esturniolo@gmail.com>
2015-08-11 17:14:58 -0300

    See description below
    Added some comments.
    Added line to create a file watched by IFTTT

--- pbbad.sh
+++ pbbad.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#Speedtest Boton made by Emiliano Sturniolo <esturniolo@gmail.com>
+#Use it as you want, be happy. Just remember me in every run :D
 
-DIRDEST=/your/dest/dir/watched/by/IFTTT
+DIRDEST=/your/Dropbox/dir/watched/by/IFTTT
 DIRTEMP=/go/to/temp/dir #Same as speedtest_boton.sh
 FILETEMP=test_temporal.tmp
 SPEEDDOWN=`awk '$0=$2' FS='Download: ' RS='Mbit/s' $DIRTEMP/$FILETEMP`
@@ -11,3 +13,4 @@ MSG="My ISP speed sucks now! Download: $SPEEDDOWN Mbit/s / Upload: $SPEEDUP Mbit
 
 curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="ALERT - ISP" -d body="$MSG"
 
+touch $DIRDEST/"Download $SPEEDDOWN- Upload $SPEEDUP"
