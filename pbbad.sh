be635d0bc4b4b43bc5663e54ef88e46a1a3a58fc
esturniolo <esturniolo@gmail.com>
2015-08-23 13:13:27 -0300

    Bugfix. Functions added.

--- pbbad.sh
+++ pbbad.sh
@@ -2,17 +2,37 @@
 #Speedtest Boton made by Emiliano Sturniolo <esturniolo@gmail.com>
 #Use it as you want, be happy. Just remember me in every run :D
 
+function copy {
+
 DIRDEST=/your/Dropbox/dir/watched/by/IFTTT
 DIRTEMP=/go/to/temp/dir #Same as speedtest_boton.sh
-FILETEMP=test_temporal.tmp
-SPEEDDOWN=`awk '$0=$2' FS='Download: ' RS='Mbit/s' $DIRTEMP/$FILETEMP`
-SPEEDDUP=`awk '$0=$2' FS='Upload: ' RS='Mbit/s' $DIRTEMP/$FILETEMP`
+FILETEMP=$DIRTEMP/ifttt_temporal.tmp
+ORIGINALTEMP=$DIRTEMP/test_temporal.tmp
 
-API="Your_Pushbullet_API_here"
-MSG="My ISP speed sucks now! Download: $SPEEDDOWN Mbit/s / Upload: $SPEEDUP Mbit/s"
+cp $ORIGINALTEMP $FILETEMP
 
-curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="ALERT - ISP" -d body="$MSG"
+SPEEDDOWN=`awk '$0=$2' FS='Download: ' RS='/s' $FILETEMP`
+SPEEDUP=`awk '$0=$2' FS='Upload: ' RS='/s' $FILETEMP`
 
-rm $DIRDEST/*.*
+}
 
-touch $DIRDEST/"Download $SPEEDDOWN- Upload $SPEEDUP"
+
+function pushbullet {
+
+  API="Your_Pushbullet_API_here"
+  MSG="My ISP speed sucks now! Download: $SPEEDDOWN Mbit/s / Upload: $SPEEDUP Mbit/s"
+  curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="ALERT - ISP" -d body="$MSG"
+
+}
+
+
+function deletecreate {
+  
+  rm $DIRDEST/Download*
+  touch $DIRTW/"Download $SPEEDDOWN - Upload $SPEEDUP"
+
+}
+
+copy
+pushbullet
+deletecreate
